04e33b2d4b1abe76bb33cd5f88844e9ff21403b8
Leshens <kacper.kamm@gmail.com>
2022-11-27 23:15:14 +0100

    update #1

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <h1>Popular web browsers</h1>
+                <h1>Opera web browsers</h1>
             </header>
         )
     }
